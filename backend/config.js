25e2161c31248af582e22859540d01155317153d
Ubuntu <ubuntu@ip-172-31-22-172.ap-south-1.compute.internal>
2026-06-18 06:22:37 +0000

    fixed github actions mongodb env

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 const config = {
   MONGODB_URL:
-    process.env.MONGODB_URI || "mongodb://mongodb:27017/ecommerce",
+    process.env.MONGODB_URL ||
+    process.env.MONGODB_URI ||
+    "mongodb://localhost:27017/ecommerce",
+
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
   PORT: 5000,
 };
--- backend/tests/product.test.js
+++ backend/tests/product.test.js
@@ -1,3 +1,4 @@
+import { jest } from "@jest/globals";
 import request from "supertest";
 import mongoose from "mongoose";
 
