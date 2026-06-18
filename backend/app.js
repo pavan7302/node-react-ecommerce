521d46af3cc4c9af5e5b9933fe293542f6139684
Ubuntu <ubuntu@ip-172-31-22-172.ap-south-1.compute.internal>
2026-06-18 07:51:57 +0000

    test pipeline

--- backend/app.js
+++ backend/app.js
@@ -11,29 +11,19 @@ import uploadRoute from "./routes/uploadRoute.js";
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// API routes
 app.use("/api/uploads", uploadRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 
-// PayPal config
 app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
-// Static files
 const __dirname = path.resolve();
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-// Catch all route
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
-});
 
 export default app;
