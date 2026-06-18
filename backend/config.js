7c94cdac3ade8ddaf0408bae7f0cc4578106da3f
Ubuntu <ubuntu@ip-172-31-22-172.ap-south-1.compute.internal>
2026-06-18 06:02:13 +0000

    fix pipeline

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,7 @@
 const config = {
   MONGODB_URL:
-    process.env.MONGO_URI ||
-    process.env.MONGODB_URL ||
-    "mongodb://localhost:27017/ecommerce",
-
+    process.env.MONGODB_URI || "mongodb://mongodb:27017/ecommerce",
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
-
   PORT: 5000,
 };
 
