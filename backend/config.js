048bcd00e7b87101819d5b908602e3733408b4c6
Ubuntu <ubuntu@ip-172-31-22-172.ap-south-1.compute.internal>
2026-06-18 15:59:23 +0000

    updated 1

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const config = {
   MONGODB_URL:
-    process.env.MONGODB_URL ||
-    process.env.MONGODB_URI ||
-    "mongodb://localhost:27017/ecommerce",
+    process.env.NODE_ENV === "test"
+      ? (process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerce")
+      : (process.env.MONGODB_URL ||
+         process.env.MONGODB_URI ||
+         "mongodb://mongodb:27017/ecommerce"),
 
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
-  PORT: 5000,
+  PORT: process.env.PORT || 5000,
 };
 
 export default config;
