cc774332e6667c23eeb9d74bf46c6061e4726789
Ubuntu <ubuntu@ip-172-31-22-172.ap-south-1.compute.internal>
2026-06-19 06:19:42 +0000

    updated app name 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
         <header className="header">
           <div className="brand">
             <button onClick={openMenu}>&#9776;</button>
-            <Link to="/">Pavan Store</Link>
+            <Link to="/">KUBEACE</Link>
           </div>
           <div className="header-links">
             <a href="cart.html">Cart</a>
